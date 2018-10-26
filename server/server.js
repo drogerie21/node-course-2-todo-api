bacf4d3ef3231f5a4b74c5765c8a68e6e1070fda
Florian Uray-Preininger <f.preininger@gmx.at>
2018-10-26 17:59:56 +0200

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -63,6 +64,33 @@ app.delete('/todos/:id', (req,res) => {
     res.status(200).send({todo});
 
   }).catch(e => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req,res)=> {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('No valid Object id');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch(e => { 
     res.status(400).send();
   });
 });
