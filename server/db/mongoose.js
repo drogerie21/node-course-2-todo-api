94f86213a790a2cfd1cd6c7f4e3e0bafa81a7062
Florian Uray-Preininger <f.preininger@gmx.at>
2018-10-26 18:37:12 +0200

    Setup seperate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp';
+const mongoURI = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise; // let mongoose use promises instead of callbacks!
 
 mongoose.connect(mongoURI, {useNewUrlParser: true});
 
 module.exports = { mongoose };
+
